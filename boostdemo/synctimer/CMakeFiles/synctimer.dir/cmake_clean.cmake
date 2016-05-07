d78eda5d811f90189479e46541fafe6c0ef01711
grefen <ccbp2003@163.com>
2016-05-07 21:16:09 +0800

    add

---
+++ boostdemo/synctimer/CMakeFiles/synctimer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/synctimer.dir/synctimer.cpp.o"
+  "synctimer.pdb"
+  "synctimer"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/synctimer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
